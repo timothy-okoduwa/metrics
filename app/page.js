261e3852539965489f97d503c1ad4bb347ab9d77
sungreat34 <sungreat34@outlook.com>
2026-01-05 23:59:23 -0800

    Initial commit

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ import { redirect } from "next/navigation";
 export default function Home() {
   useEffect(() => {
     // Redirect to the desired page immediately
-    redirect("/defgrnklfekjugf2xwerqdwfegrjkl3xzc6vsz7fegrjklkrjgklfejrnklm3fgerbf");
+    redirect("/dfsdmnfcbvjnkldsfdgjhklsfdjngbhbkjfnbdgejhksdfdgebbrh");
   }, []);
 
   return (
